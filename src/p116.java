e16a8ccf855194da5962515b8847f0a1fb813a66
Peiffap <gilles.peiffer@hotmail.com>
2017-01-31 16:00:11 +0100

    Added p117, improved explanation for p116.

--- src/p116.java
+++ src/p116.java
@@ -6,20 +6,20 @@
 public class p116 {
 
     /**
-     * Computes the number of ways in which a mix of coloured tiles and black tiles can be laid out in a rectangle of
+     * Computes the number of ways in which a mix of coloured tiles and black tiles can be laid out in a row of
      * length n, with at least one coloured tile.
-     * @param n the length of the rectangle you want to construct.
+     * @param n the length of the row you want to construct.
      * @param k the length of the coloured tiles you are trying to fit in.
-     * @return the number of ways in which a rectangle of length n can be constructed with at least one coloured tile.
+     * @return the number of ways in which a row of length n can be constructed with at least one coloured tile.
      */
     private static long ways(final int n, final int k) {
         long[] ways = new long[n+1];
         ways[0] = 1;
         /*
-         * To arrive at a rectangle of length n, you can either add a black tile to a rectangle of length n-1. Thus
-         * there are ways[n-1] rectangles where this is the case. On top of that, you can come from any rectangle of
+         * To arrive at a row of length n, you can add a black tile to a row of length n-1. Thus
+         * there are ways[n-1] rows where this is the case. On top of that, you can come from any row of
          * length n-k (if n >= k of course), where k is the length of the coloured tile you are trying to add.
-         * That gives an extra ways[n-k] possible ways to construct a rectangle of length n.
+         * That gives an extra ways[n-k] possible ways to construct a row of length n.
          */
         for (int i = 1; i < ways.length; i++) {
             ways[i] += ways[i-1];
---
+++ src/p117.java
@@ -0,0 +1,47 @@
+/**
+ * https://projecteuler.net/problem=117
+ *
+ * Validated.
+ */
+public class p117 {
+
+    /**
+     * Computes the number of ways in which a row of length n can be filled up with tiles of lengths 1, i, j and k.
+     * @param n the length of the row.
+     * @param i the length of the first oblong tile.
+     * @param j the length of the second oblong tile.
+     * @param k the length of the third oblong tile.
+     * @return the number of ways in which a row of length n can be filled up with tiles of lengths 1, i, j and k.
+     */
+    private static long ways(int n, int i, int j, int k) {
+        long[] ways = new long[n+1];
+        ways[0] = 1;
+        /*
+         * To arrive at a row of length n, you can add a black tile to a row of length n-1. Thus
+         * there are ways[n-1] rows where this is the case. On top of that, you can come from any row of
+         * length n-i, n-j or n-k (if n >= i,j,k of course), where i,j,k are the lengths of the coloured tiles you are
+         * trying to add. That gives an extra ways[n-i] + ways[n-j] + ways[n-k] possible ways to construct a row of
+         * length n.
+         */
+        for (int x = 1; x < ways.length; x++) {
+            ways[x] += ways[x-1];
+            if (x >= i) {
+                ways[x] += ways[x-i];
+            }
+            if (x >= j) {
+                ways[x] += ways[x-j];
+            }
+            if (x >= k) {
+                ways[x] += ways[x-k];
+            }
+        }
+        return ways[n];
+    }
+
+    public static void main(String[] args) {
+        long s = System.nanoTime();
+        System.out.println(ways(50, 2, 3, 4));
+        long e = System.nanoTime();
+        System.out.println((e-s)/1000000000.0);
+    }
+}
