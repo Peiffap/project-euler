42728f1e0a6915d3d1f63c8b3ff11bde05aa5ded
Peiffap <gilles.peiffer@hotmail.com>
2017-01-31 15:04:29 +0100

    Added p187.

---
+++ src/p187.java
@@ -0,0 +1,28 @@
+/**
+ * https://projecteuler.net/problem=187
+ *
+ * Validated.
+ */
+public class p187 {
+    public static void main (String[] args) {
+        long s = System.nanoTime();
+        boolean[] primes = Library.primeTab(100000000);
+        boolean[] semiprimes = new boolean[100000000];
+        for (int i = 2; i <= Math.sqrt(primes.length); i++) {
+            for (int j = i; j*i < primes.length; j++) {
+                if (primes[i] && primes[j]) {
+                    semiprimes[i*j] = true;
+                }
+            }
+        }
+        int count = 0;
+        for (boolean b : semiprimes) {
+            if (b) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        long e = System.nanoTime();
+        System.out.println((e-s)/1000000000.0);
+    }
+}
