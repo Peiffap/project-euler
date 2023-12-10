9bcff3546321cdeac64141ae88ebb4abccd7d020
Gilles Peiffer <gilles.peiffer.yt@gmail.com>
2023-12-10 14:29:41 +0100

    Improve the sieving for the Java solution of problem 146

--- src/p146.java
+++ src/p146.java
@@ -22,9 +22,23 @@ class p146 {
     public static void main(String[] args) {
         long s = System.nanoTime();
         int sum = 0;
-        for (int i = 10; i < 1000000; i+=10) { // n%10 has to be 0.
-            if(primeGen(i)) {
-                sum += i;
+        // if n%2 = 1, n^2 + 1 = 2k; not prime
+        // if n%5 = [1, 2, 3, 4], n^2 + [9, 3, 7, 1] = 5k; not prime
+        // hence n has to be a multiple of 10
+        // similarly, we need n%3 = [2, 3] and n%7 = [3, 4]
+        // combining all of this, we see that we need n%210 = [10, 80, 130, 200]
+        for (int i = 0; i < 150000000; i += 210) {
+            if (primeGen(i + 10)) {
+                sum += i + 10;
+            }
+            if (primeGen(i + 80)) {
+                sum += i + 80;
+            }
+            if (primeGen(i + 130)) {
+                sum += i + 130;
+            }
+            if (primeGen(i + 200)) {
+                sum += i + 200;
             }
         }
         System.out.println(sum);
