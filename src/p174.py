9162b6302034b540f5345384d36f0532ccb4a504
Gilles Peiffer <gilles.peiffer.yt@gmail.com>
2023-12-12 13:39:51 +0100

    Add Python solution for problem 174

---
+++ src/p174.py
@@ -0,0 +1,25 @@
+# https://projecteuler.net/problem=174
+
+import time
+
+start = time.time()
+
+l = 1000000 # maximum number of tiles
+l4 = l // 4
+ways = [0] * (l + 1)
+m = 1 # thickness of the hsl
+n_max = l4 // m + m # n^2 - (n - 2m)^2 <= l -> n <= (l / 4 + m^2) / m
+while n_max >= 2 * m: # while n is sufficient to have thickness m
+    for i in range(2 * m + 1, n_max + 1): # for all possible outer lengths at this thickness
+        ways[4 * m * (i - m)] += 1 # add 1 to total tiles used
+    m += 1
+    n_max = l4 // m + m
+
+n = 0 # number of hsl with total tile count less than or equal to 10
+for i in range(1, 11):
+    n += ways.count(i) # add number of hsl with total tile count equal to i
+
+print(n)
+
+end = time.time()
+print(end - start)
